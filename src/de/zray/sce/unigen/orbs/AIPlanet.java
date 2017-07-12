001f2439737437661144a6cf53c98e7b220f74d3
Vortex Acherontic <vortex@z-ray.de>
2017-07-12 17:18:43 +0200

    Trying to fix planet behaviour in SCEMainWorld

--- src/de/zray/sce/unigen/orbs/AIPlanet.java
+++ src/de/zray/sce/unigen/orbs/AIPlanet.java
@@ -22,15 +22,15 @@ public class AIPlanet extends SEAI{
     
     public AIPlanet(SEWorld world, SEActor actor, SEAIWorld aiMod) {
         super(world, actor, aiMod);
-        distToSun = (float) (Math.random()*100);
+        distToSun = (float) (Math.random()*10);
         day = (float) (Math.random()*10);
         year = (float) Math.random();
     }
 
     @Override
     public void act(double delta) {
-        curDay = (curDay+day*delta)%360;
-        curYear = (curYear+year*delta)%360;
+        curDay = (10*delta)%360;
+        curYear = (10*delta)%360;
         Vector2d cords = SEUtils.calcCoordinates(distToSun, curYear);
         parrentActor.getOrientation().setPosition(cords.x, 0, cords.y);
         parrentActor.getOrientation().setRotation(0, curDay, 0);
