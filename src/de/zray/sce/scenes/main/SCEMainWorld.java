0a3d4de448a5f95281c72799f6d9aab4d79b4dd7
Vortex Acherontic <vortex@z-ray.de>
2017-07-12 02:11:34 +0200

    Updated SCEMainWorld meshes to be rendered as VBO

--- src/de/zray/sce/scenes/main/SCEMainWorld.java
+++ src/de/zray/sce/scenes/main/SCEMainWorld.java
@@ -11,6 +11,7 @@ import de.zray.sce.unigen.orbs.Planet;
 import de.zray.se.SEActor;
 import de.zray.se.SEWorld;
 import de.zray.se.audio.AudioSource;
+import de.zray.se.grapics.Camera;
 import de.zray.se.grapics.semesh.SEMaterial;
 import de.zray.se.grapics.modelloader.Modelloader;
 import de.zray.se.grapics.semesh.SEMesh;
@@ -26,10 +27,17 @@ public class SCEMainWorld extends SEWorld{
     @Override
     public void init(){
         //addGUI(new GUIMain(this));
+        Camera cam = new Camera(this);
+        cam.setPerspectiveRendering(true);
+        cam.setViewMode(Camera.ViewMode.EGO);
+        cam.setRotation(90, 0, 0);
+        int mainCam = this.addCamera(cam);
+        this.setActiveCamera(mainCam);
         
         SEMesh stationMesh = Modelloader.get().loadModel("scedata/models/cron/warpstation/warpstation.obj");
         stationMesh.setMaterial(new SEMaterial("scedata/models/cron/warpstation/warpstation.jpg"));
         stationMesh.setRenderDist(50);
+        stationMesh.setRenderMode(SEMesh.RenderMode.VBO);
         SEActor station = new SEActor(stationMesh, null, null, this);
         station.setAI(new AIStation(this, station, this.getAIWorld()));
         station.setOriantation(new SEOriantation(0, 0, -20));
@@ -37,21 +45,25 @@ public class SCEMainWorld extends SEWorld{
         SEMesh stationLOD0_5 = Modelloader.get().loadModel("scedata/models/cron/warpstation/warpstation-0.5.obj");
         stationLOD0_5.setMaterial(new SEMaterial("scedata/models/cron/warpstation/warpstation.jpg"));
         stationLOD0_5.setRenderDist(70);
+        stationLOD0_5.setRenderMode(SEMesh.RenderMode.VBO);
         stationMesh.addLOD(stationLOD0_5);
         
         SEMesh stationLOD0_25 = Modelloader.get().loadModel("scedata/models/cron/warpstation/warpstation-0.25.obj");
         stationLOD0_25.setMaterial(new SEMaterial("scedata/models/cron/warpstation/warpstation.jpg"));
         stationLOD0_25.setRenderDist(80);
+        stationLOD0_25.setRenderMode(SEMesh.RenderMode.VBO);
         stationMesh.addLOD(stationLOD0_25);
         
         SEMesh stationLOD0_0 = Modelloader.get().loadModel("scedata/models/cron/warpstation/warpstation-0.0.obj");
         stationLOD0_0.setMaterial(new SEMaterial("scedata/models/cron/warpstation/warpstation.jpg"));
         stationLOD0_0.setRenderDist(100);
+        stationLOD0_0.setRenderMode(SEMesh.RenderMode.VBO);
         stationMesh.addLOD(stationLOD0_0);
         addSEActor(station);
         
         SEMesh lycanMesh = Modelloader.get().loadModel("scedata/models/wolfrim/lycan/lycan.obj");
         lycanMesh.setMaterial(new SEMaterial("scedata/models/wolfrim/lycan/lycan.png"));
+        lycanMesh.setRenderMode(SEMesh.RenderMode.VBO);
         SEActor lycan = new SEActor(lycanMesh, null, null, this);
         lycan.setAI(new AILycan(this, lycan, this.getAIWorld()));
         addSEActor(lycan);
