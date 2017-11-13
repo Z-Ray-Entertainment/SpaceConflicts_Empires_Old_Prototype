b92a18f2315a3bd570a5ddc3b585a51e850cdf7a
Vortex Acherontic <vortex@z-ray.de>
2017-11-13 18:43:13 +0100

    Added switchable start scenes

--- src/de/zray/sce/SCEMain.java
+++ src/de/zray/sce/SCEMain.java
@@ -6,6 +6,7 @@
 package de.zray.sce;
 
 import de.zray.sce.scenes.main.SCEMainWorld;
+import de.zray.sce.scenes.test.TextureTest;
 import de.zray.se.MainThread;
 import de.zray.se.Settings;
 import de.zray.se.ai.SEAIWorld;
@@ -17,28 +18,39 @@ import java.io.IOException;
  * @author vortex
  */
 public class SCEMain {
+    private static SCEMain sceMain = new SCEMain();
+    
     public static void main(String[] args) throws IOException{
         Settings.get().debug.debugMode = Settings.DebugMode.DEBUG_AND_OBJECTS;
         Settings.get().window.resX = 1280;
         Settings.get().window.resY = 720;
         Settings.get().version = de.zray.sce.game.Settings.version+" "+de.zray.sce.game.Settings.suffix;
         Settings.get().title = de.zray.sce.game.Settings.name;
-        //test();
-        SCEMainWorld mainWorld = new SCEMainWorld();
-        SEAIWorld aiWorld = new SEAIWorld(mainWorld);
-        SEAudioWorld audioWorld = new SEAudioWorld(mainWorld);
-        mainWorld.setAIWorld(aiWorld);
-        mainWorld.setAudioWorld(audioWorld);
-        mainWorld.init();
         
-        final MainThread mainThread = new MainThread();
-        mainThread.setRenderBackend(new GLRenderer());
-        mainThread.switchWorld(mainWorld);
-        
-        mainThread.loop();
+        sceMain.initSCE(1);
     }
     
-    private static void test(){
+    private void initSCE(int scene) throws IOException{
+        final MainThread mainThread = new MainThread();
+        mainThread.setRenderBackend(new GLRenderer());
         
+        switch(scene){
+            case 0 :
+                SCEMainWorld mainWorld = new SCEMainWorld();
+                SEAIWorld aiWorld = new SEAIWorld(mainWorld);
+                SEAudioWorld audioWorld = new SEAudioWorld(mainWorld);
+                mainWorld.setAIWorld(aiWorld);
+                mainWorld.setAudioWorld(audioWorld);
+                mainWorld.init();
+                mainThread.switchWorld(mainWorld);
+                break;
+            case 1 :
+                TextureTest textureTest = new TextureTest();
+                textureTest.init();
+                mainThread.switchWorld(textureTest);
+                break;
+        }
+        
+        mainThread.loop();
     }
 }
