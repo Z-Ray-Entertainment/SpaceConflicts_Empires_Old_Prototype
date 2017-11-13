c66998911c2855ce39da71c5e5ce80c540dfbd55
Vortex Acherontic <vortex@z-ray.de>
2017-11-13 19:01:21 +0100

    #1 SingularityEngine#11 Adding test scene for Texture issue to SCE

--- src/de/zray/sce/scenes/test/TextureTest.java
+++ src/de/zray/sce/scenes/test/TextureTest.java
@@ -5,10 +5,12 @@
  */
 package de.zray.sce.scenes.test;
 
+import de.zray.sce.scenes.main.SpectatorInput;
 import de.zray.se.SEActor;
 import de.zray.se.SEWorld;
-import de.zray.se.grapics.semesh.SEMesh;
-import de.zray.se.grapics.shapes.Plane;
+import de.zray.se.graphics.semesh.SEMesh;
+import de.zray.se.graphics.shapes.Plane;
+import de.zray.se.graphics.Camera;
 
 /**
  *
@@ -18,7 +20,14 @@ public class TextureTest extends SEWorld{
 
     @Override
     public void init() {
+        addInputManager(new SpectatorInput(this));
         SEMesh plane = new Plane(1, 1, false).getSEMesh();
+        Camera cam = new Camera();
+        cam.setPerspectiveRendering(true);
+        cam.setViewMode(Camera.ViewMode.EGO);
+        cam.setRotation(90, 0, 0);
+        int camId= addCamera(cam);
+        setActiveCamera(camId);
         
         addSEActor(new SEActor(plane, null, null, this));
     }
